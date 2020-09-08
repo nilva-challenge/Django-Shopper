7208ad7472101fadd3e16583fb48daaf11434be2
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-08 21:53:07 +0430

    define userprofile model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    pass
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users"""
+    email = models.EmailField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # this field for access to django admin
+    objects = UserProfileManager()
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        """Return string of users"""
+        return self.email
