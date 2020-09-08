24bd221a919a20bb17e4dfae89ecec311b36c317
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-08 22:46:45 +0430

    create UserProfileManager class

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -3,7 +3,25 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 
 class UserProfileManager(BaseUserManager):
-    pass
+    """Manager for user"""
+
+    def create_user(self, email, first_name, last_name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('User must have an email')
+        email = self.normalize_email(email=email)  # Normalizes email addresses by lowercasing the domain portion of the email address.
+        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)  # set_password for encrypting password (hash password)
+        user.save(using=self._db)  # if django has multiple database
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password=None):
+        """Creates and saves a superuser with the given email, date of"""
+        user = self.create_user(email=email, first_name=first_name, last_name=last_name)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
