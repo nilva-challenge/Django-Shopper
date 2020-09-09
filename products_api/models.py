ac4a9f70337ba6ac7c73214e993d4fc64f6c1b16
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-09 11:23:41 +0430

    create Product and Order model

--- products_api/models.py
+++ products_api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from profiles_api.models import UserProfile
+from datetime import datetime
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=500)
+    count = models.IntegerField(default=1, blank=False, null=False)
+    price = models.IntegerField(blank=False, null=False)
+    create_date = models.DateTimeField(blank=False, default=datetime.now)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    count = models.IntegerField(blank=False, null=False)
+    order_date = models.DateTimeField(default=datetime.now, blank=False, null=False)
