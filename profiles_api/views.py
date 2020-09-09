e73cf2f7b91e219f373ff2dc5ea5fa0696fe39ac
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-09 13:04:09 +0430

    authentication_user method for create token ang login user

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from .models import UserProfile
+from rest_framework_jwt.serializers import jwt_payload_handler
+from django.contrib.auth.signals import user_logged_in
+from rest_framework.response import Response
+from rest_framework import status
+import jwt
+from django_shopper import settings
 
-# Create your views here.
+
+@api_view(['POST'])  # this method  works with post
+@permission_classes([AllowAny, ])  # any body can access to this method
+def authenticate_user(request):
+    """Create token for next steps"""
+    try:
+        email = request.data['email']
+        password = request.data['password']
+        if email == '' or password == '':
+            res = {'خطا': 'ایمیل و پسورد نمی توانند خالی باشند.'}
+            return Response(res, status=status.HTTP_403_FORBIDDEN)
+
+        user = UserProfile.objects.get(email=email)
+        if user.check_password(password):
+            try:
+                payload = jwt_payload_handler(user)
+                token = jwt.encode(payload, settings.SECRET_KEY)
+                user_details = {}
+                user_details['user'] = user.email
+                user_details['token'] = token
+                user_logged_in.send(sender=user.__class__, request=request, user=user)
+                return Response(user_details, status=status.HTTP_200_OK)
+
+            except Exception as e:
+                raise e
+        else:
+            res = {'error': 'can not authenticate with the given credentials or the account has been deactivated'}
+            return Response(res, status=status.HTTP_403_FORBIDDEN)
+    except KeyError:
+        res = {'خطا': 'افزودن فیلد های ایمیل و پسورد اجباری می باشند.'}
+        return Response(res)
