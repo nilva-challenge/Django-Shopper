de3a883c59d576b58cf3292212090d4d0e75b78d
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-09 11:11:47 +0430

    add products_api into installed_app

--- django_shopper/settings.py
+++ django_shopper/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'profiles_api',
     'rest_framework',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'products_api'
 ]
 
 MIDDLEWARE = [
