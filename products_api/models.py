8fade6a84021a26f7411553056c464197803944b
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-09 12:54:58 +0430

    edit __str__ method in products_api model and add comments

--- products_api/models.py
+++ products_api/models.py
@@ -4,14 +4,22 @@ from datetime import datetime
 
 
 class Product(models.Model):
+    """Database model for products"""
     name = models.CharField(max_length=500)
-    count = models.IntegerField(default=1, blank=False, null=False)
-    price = models.IntegerField(blank=False, null=False)
+    count = models.PositiveIntegerField(default=1, blank=False, null=False)
+    price = models.PositiveIntegerField(blank=False, null=False)
     create_date = models.DateTimeField(blank=False, default=datetime.now)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
+    """Database model for orders of users"""
     user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    count = models.IntegerField(blank=False, null=False)
+    count = models.PositiveIntegerField(blank=False, null=False)
     order_date = models.DateTimeField(default=datetime.now, blank=False, null=False)
+
+    def __str__(self):
+        return self.product
