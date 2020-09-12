4cedbe9d78b54afc54e020f7f4810738ab69dee1
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-12 14:26:59 +0430

    add methods for authenticate and profile user

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
-from .models import UserProfile
+from .models import User
 from rest_framework_jwt.serializers import jwt_payload_handler
 from django.contrib.auth.signals import user_logged_in
 from rest_framework.response import Response
@@ -8,6 +8,11 @@ from rest_framework import status
 import jwt
 from django_shopper import settings
 from .serializers import UserProfileSerializer
+from .permissions import IsOwner, IsSuperUser
+from rest_framework import viewsets
+from rest_framework import filters
+from django_shopper.messages import success, errors
+from django.contrib.auth import authenticate
 
 
 @api_view(['POST'])  # this method  works with post
@@ -17,41 +22,57 @@ def authenticate_user(request):
     try:
         email = request.data['email']
         password = request.data['password']
-        if email == '' or password == '':
-            res = {'خطا': 'ایمیل و پسورد نمی توانند خالی باشند.'}
-            return Response(res, status=status.HTTP_403_FORBIDDEN)
-
-        user = UserProfile.objects.filter(email=email)
-        if user.exists():
-            if user[0].check_password(password):
-                user = user[0]
-                try:
-                    payload = jwt_payload_handler(user)
-                    token = jwt.encode(payload, settings.SECRET_KEY)
-                    user_details = {}
-                    user_details['user'] = user.email
-                    user_details['token'] = token
-                    user_logged_in.send(sender=user.__class__, request=request, user=user)
-                    return Response(user_details, status=status.HTTP_200_OK)
-
-                except Exception as e:
-                    raise e
-            else:
-                res = {'خطا': 'ایمیل و پسورد را صحیح وارد کنید.'}
-                return Response(res)
-        else:
+        if email == '' or password == '':  # validate
+            code = 100
+            error = {
+                'code': code,
+                'msg': errors[code]
+            }
+            return Response(error, status.HTTP_400_BAD_REQUEST)
+        user_authenticate = authenticate(email=email, password=password)  # check if user there is in database
+        if user_authenticate is not None:
+            try:
+                payload = jwt_payload_handler(user_authenticate)
+                token = jwt.encode(payload, settings.SECRET_KEY)
+                user_details = {}
+                user_details['id'] = user_authenticate.id
+                user_details['user'] = user_authenticate.email
+                user_details['token'] = token
+                user_logged_in.send(sender=user_authenticate.__class__, request=request, user=user_authenticate)
+                return Response(user_details, status=status.HTTP_200_OK)
+            except Exception as e:
+                raise e
+        else:  # if email user there is not in database,it will be created..
             result = create_user(request)
-
             if result == 1:
-                res = {'پیغام': 'ثبت نام شما با موفقیت انجام شد.'}
-                return Response(res, status=status.HTTP_200_OK)
+                code = 201
+                success_msg = {
+                    'code': code,
+                    'msg': success[code]
+                }
+                return Response(success_msg, status=status.HTTP_201_CREATED)
+            elif result == 2:
+                code = 105
+                error = {
+                    'code': code,
+                    'msg': errors[code]
+                }
+                return Response(error, status.HTTP_400_BAD_REQUEST)
             else:
-                res = {'پیغام': 'با خطا مواجه شده اید.'}
-                return Response(res, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                code = 104
+                error = {
+                    'code': code,
+                    'msg': errors[code]
+                }
+                return Response(error, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     except KeyError:
-        res = {'خطا': 'افزودن فیلد های ایمیل و پسورد اجباری می باشند.'}
-        return Response(res)
+        code = 104
+        error = {
+            'code': code,
+            'msg': errors[code]
+        }
+        return Response(error, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @permission_classes([AllowAny, ])  # any body can access to this method
@@ -63,7 +84,26 @@ def create_user(request):
         if serializer.is_valid():
             email = serializer.validated_data.get('email')
             password = serializer.validated_data.get('password')
-            UserProfile.objects.create_user(email=email, password=(password))
+            User.objects.create_user(email=email, password=password)
             return 1
+        else:
+            return 2  # is not valid email or password
+
     except:
         return 0
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """create user profile and update profile"""
+    serializer_class = UserProfileSerializer
+    queryset = User.objects.all()
+
+    def get_permissions(self):  # this method is for define user permission for updating own profile
+        if self.action == 'list':
+            self.permission_classes = [IsSuperUser, ]
+        elif self.action == 'retrieve':
+            self.permission_classes = [IsOwner]
+        return super(self.__class__, self).get_permissions()
+
+    filter_backends = (filters.SearchFilter,)  # in browser there is filter base on name and email in list ,and admin user has permission to see list of product
+    search_fields = ('name', 'email',)
