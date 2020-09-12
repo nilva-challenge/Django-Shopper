0a3c43057d53235d07b99d4d6ff9606036e7eeaa
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-12 14:56:32 +0430

    add read_only for email field

---
+++ profiles_api/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from .models import User
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializers a user profile object"""
+
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'first_name', 'last_name')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {
+                    'input_type': 'password'
+                }},  # when read dont show password
+            'email': {
+                'read_only': True,  # do not permit to change email
+
+            }
+        }  # convert model instance into python object with serializer
