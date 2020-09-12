2858c0c5a8a28ebf48c1e31a5e8bc38c855c71e7
arezoo <arezoo.darvishi69@yahoo.com>
2020-09-12 15:01:01 +0430

    edit file for rule pep8

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,39 +1,34 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager, AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
 
-class UserProfileManager(BaseUserManager):
+class UserManager(BaseUserManager):
     """Manager for user"""
 
-    def create_user(self, email, first_name, last_name, password=None):
+    def create_user(self, email, password, **extra_fields):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email')
         email = self.normalize_email(email=email)  # Normalizes email addresses by lowercasing the domain portion of the email address.
-        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)  # set_password for encrypting password (hash password)
         user.save(using=self._db)  # if django has multiple database
         return user
 
-    def create_superuser(self, email, first_name, last_name, password=None):
+    def create_superuser(self, email, password, **extra_fields):
         """Creates and saves a superuser with the given email, date of"""
-        user = self.create_user(email=email, first_name=first_name, last_name=last_name)
+        user = self.create_user(email=email, password=password, **extra_fields)
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
         return user
 
 
-class UserProfile(AbstractBaseUser, PermissionsMixin):
-    """Database model for users"""
-    email = models.EmailField(max_length=255, unique=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)  # this field for access to django admin
-    objects = UserProfileManager()
+class User(AbstractUser):
+    """User model."""
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
     USERNAME_FIELD = 'email'
-
-    def __str__(self):
-        """Return string of users"""
-        return self.email
+    REQUIRED_FIELDS = []
+    objects = UserManager()
