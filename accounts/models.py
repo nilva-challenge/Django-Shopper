584b7a6095f4016083de0465ed53c10b4c887b98
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2020-09-14 14:44:42 +0430

    Added CustomUser model for using email as username

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from accounts.managers import CustomUserManager
+
+class CustomUser(AbstractUser):
+    """
+    Modified User model for using email address as username.
+    """
+    username = None
+    email = models.EmailField(_("Email Address"), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
