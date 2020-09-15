dbce3d028dfbbb7887521656026e9a4dd2dcc089
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2020-09-15 12:44:48 +0430

    Added Poduct model, serializer, manager, list endpoint

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'count', )
+    search_fields = ('title', )
+    ordering = ('count', )
+
--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from products.managers import ProductManager
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(blank=True, null=True)
+    price = models.FloatField(default=0.0)
+    count = models.IntegerField(default=0)
+
+    objects = ProductManager()
+
+    def __str__(self):
+        return self.title
