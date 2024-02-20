3dba2a75a8fcea552a881eb1e2c8866fe19fa62b
ali <alisfryly075@gmail.com>
2024-02-20 23:30:16 +0330

    fix auth model

--- identity/models.py
+++ identity/models.py
@@ -1,27 +1,10 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
-class UserManager(BaseUserManager):
-
-    def create_superuser(self, email, first_name, last_name, password=None, **extra_fields):
-        return self.create_superuser(email, first_name, last_name, password, **extra_fields)
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    is_active = models.BooleanField(default=True)
-    is_staff = models.BooleanField(default=False)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), unique=True)
 
     def __str__(self):
         return self.email
