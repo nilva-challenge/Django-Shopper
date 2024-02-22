809b56ea67775fc00641b31caa55a649321a2059
ali <alisfryly075@gmail.com>
2024-02-22 13:00:10 +0330

    commit

--- shop/views.py
+++ shop/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
-from django.shortcuts import get_object_or_404
 
 from .serializers import *
 
